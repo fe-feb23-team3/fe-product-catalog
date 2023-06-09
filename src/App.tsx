403d2753bff3e4d5d84841002ee6400c47340cad
Maksym Didyk <didyk.maksym@gmail.com>
2023-06-09 15:09:12 +0300

    phone card v.1

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,11 @@
 import React from 'react';
 import './App.scss';
-import image from './images/beaver_logo.jpg';
+import { Card } from './components/Card';
 
 export const App: React.FC = () => {
   return (
     <>
-      <img
-        src={image}
-        alt="beaver"
-        className="logo"
-      />
-      <div className="cover">
-        <h1 className="title">There will be a project soon...</h1>
-      </div>
+      <Card />
     </>
   );
 };
