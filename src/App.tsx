031d2bf30549fd6799e8c81df475333fdee73d8e
Stas Lukianchuk <stas.lukianchuk@icloud.com>
2023-06-09 11:29:45 +0300

    add react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import './App.scss';
 import image from './images/beaver_logo.jpg';
+import { HomePage } from './HomePage';
+import { PhoneCatalog } from './PhoneCatalog';
 
 export const App: React.FC = () => {
   return (
@@ -13,6 +16,11 @@ export const App: React.FC = () => {
       <div className="cover">
         <h1 className="title">There will be a project soon...</h1>
       </div>
+
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/phones" element={<PhoneCatalog />} />
+      </Routes>
     </>
   );
 };
