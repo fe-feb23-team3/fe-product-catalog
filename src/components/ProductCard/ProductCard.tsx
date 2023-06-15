cc4624ae324b52d4a85d75432371642cbae8b50e
Nazarii Chepil <nazzikk1998@gmail.com>
2023-06-16 00:03:41 +0300

    fixed product card and implemented animation

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -27,17 +27,21 @@ export const ProductCard: React.FC<Props> = ({
   return (
     <div className="card">
       <div className="card__container">
-        <NavLink to={`/phoneCardData/${itemId}`} className="item__card__link">
-          <div className="image__wrapper">
-            <img
-              src={`https://be-product-catalog.onrender.com/products/phones/${id}/image`}
-              alt="Apple iPhone 14 Pro 128GB Silver (MQ023)"
-              className="card__photo"
-            />
-          </div>
+        <div className="card__top-wrapper">
+          <NavLink to={`/phoneCardData/${itemId}`} className="item__card__link">
+            <div className="image__wrapper">
+              <img
+                src={`https://be-product-catalog.onrender.com/products/phones/${id}/image`}
+                alt="Apple iPhone 14 Pro 128GB Silver (MQ023)"
+                className="card__photo"
+              />
+            </div>
 
-          <h2 className="card__title">{name}</h2>
+            <h2 className="card__title">{name}</h2>
+          </NavLink>
+        </div>
 
+        <div className="card__bottom-wrapper">
           <div className="card__price">
             <span className="card__price-current">{`$${price}`}</span>
             {!showDiscount && (
@@ -52,39 +56,41 @@ export const ProductCard: React.FC<Props> = ({
               <p className="card__spec-title">Screen</p>
               <p className="card__spec-value">{screen}</p>
             </div>
+
             <div className="card__spec">
               <p className="card__spec-title">Capacity</p>
               <p className="card__spec-value">{capacity}</p>
             </div>
+
             <div className="card__spec">
               <p className="card__spec-title">RAM</p>
               <p className="card__spec-value">{ram}</p>
             </div>
+
+            <div className="card__buttons">
+              <button
+                type="button"
+                onClick={() => handleAddToCart(id)}
+                className={classNames('card__buttons-addToCart', {
+                  'card__buttons-addToCart-checked': itemsCart.includes(id),
+                })}
+              >
+                {itemsCart.includes(id) ? 'Added to cart' : 'Add to cart'}
+              </button>
+
+              <button
+                type="button"
+                onClick={() => handleAddToFavourites(id)}
+                className={classNames('card__buttons-AddToFavourites', {
+                  'card__buttons-AddToFavourites-default':
+                    !itemsFavourites.includes(id),
+                  'card__buttons-AddToFavourites-checked':
+                    itemsFavourites.includes(id),
+                })}
+              >
+              </button>
+            </div>
           </div>
-        </NavLink>
-
-        <div className="card__buttons">
-          <button
-            type="button"
-            onClick={() => handleAddToCart(id)}
-            className={classNames('card__buttons-addToCart', {
-              'card__buttons-addToCart-checked': itemsCart.includes(id),
-            })}
-          >
-            {itemsCart.includes(id) ? 'Added to cart' : 'Add to cart'}
-          </button>
-
-          <button
-            type="button"
-            onClick={() => handleAddToFavourites(id)}
-            className={classNames('card__buttons-AddToFavourites', {
-              'card__buttons-AddToFavourites-default':
-                !itemsFavourites.includes(id),
-              'card__buttons-AddToFavourites-checked':
-                itemsFavourites.includes(id),
-            })}
-          >
-          </button>
         </div>
       </div>
     </div>
