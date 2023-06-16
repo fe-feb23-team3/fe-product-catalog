9a9e994e5d2d46d06a35d1d8378b9ab520daf98f
Nikita Zadorozhnyi <79592253+Zad0rnik@users.noreply.github.com>
2023-06-16 10:18:09 +0300

    small fixes (#57)

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -24,8 +24,8 @@ export const Footer: React.FC = () => {
           <NavLink to="https://w.forfun.com/fetch/b0/b0ebdaa1402892e90d28fad4e3190d22.jpeg" target="_blank" className="footer__link">
             contacts
           </NavLink>
-          
-          <NavLink to="https://w.forfun.com/fetch/e4/e46916f00da4bf755047f02b4560eb0c.jpeg" target="_blank" className="footer__link"
+
+          <NavLink to="https://w.forfun.com/fetch/e4/e46916f00da4bf755047f02b4560eb0c.jpeg" target="_blank" className="footer__link">
             rights
           </NavLink>
         </div>
