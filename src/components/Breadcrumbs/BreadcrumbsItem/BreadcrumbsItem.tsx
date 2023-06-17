ec09615c904e188e85447d6d8fe6ef977a14973d
Nazarii Chepil <nazzikk1998@gmail.com>
2023-06-17 16:39:05 +0300

    created Breadcrumbs

---
+++ src/components/Breadcrumbs/BreadcrumbsItem/BreadcrumbsItem.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import arrowRight from '../../images/arrow_grey_right.svg';
+import './BreadcrumbsItem.scss';
+
+interface Props {
+  text: string;
+}
+
+export const BreadcrumbsItem: React.FC<Props> = ({ text }) => {
+  return (
+    <div className="breadcrumbsItem">
+      <img
+        className="breadcrumbsItem-icon"
+        src={arrowRight}
+        alt="arrowRight"
+      />
+      <span className="breadcrumbsItem-link">{text}</span>
+    </div>
+  );
+};
