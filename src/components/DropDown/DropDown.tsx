5c14be1d328a1714ec7aee360a2be9b44912c52d
Nazarii Chepil <nazzikk1998@gmail.com>
2023-06-20 18:05:10 +0300

    created DropDown component and used him inside ProductCard

---
+++ src/components/DropDown/DropDown.tsx
@@ -0,0 +1,83 @@
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+import React, { useEffect, useRef, useState } from 'react';
+import './DropDown.scss';
+import classNames from 'classnames';
+import arrowUp from '../../images/arrow_up.svg';
+import arrowDown from '../../images/arrow_down.svg';
+
+interface Props {
+  currentItem: string;
+  items: { id: number, value: string; title: string }[];
+  onSelect: (sort: string) => void;
+}
+
+export const Dropdown: React.FC<Props> = ({ currentItem, items, onSelect }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const title = items.find((item) => item.value === currentItem)?.title || 'Choose sort';
+  const dropdownRef = useRef<HTMLDivElement>(null);
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const handleSeclect = (sort: string) => () => {
+    onSelect(sort);
+    setIsOpen(false);
+  };
+
+  useEffect(() => {
+    setIsOpen(false);
+
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        containerRef.current
+        && dropdownRef.current
+        && !dropdownRef.current.contains(event.target as Node)
+        && !containerRef.current.contains(event.target as Node)
+      ) {
+        setIsOpen(false);
+      }
+    };
+
+    document.addEventListener('mousedown', handleClickOutside);
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [currentItem]);
+
+  return (
+    <div className={classNames('dropdown', { 'dropdown--open': isOpen })}>
+      <div
+        className="dropdown__toggle"
+        onClick={toggleDropdown}
+      >
+        <div className="dropdown__toggle-text">{title}</div>
+        <div className="dropdown__toggle-icon">
+          {isOpen ? (
+            <img src={arrowUp} alt="arrow-left" className="back-link__icon" />
+          ) : (
+            <img src={arrowDown} alt="arrow-left" className="back-link__icon" />
+          )}
+        </div>
+      </div>
+      {isOpen && (
+        <ul className="dropdown__menu">
+          {items.map((item) => (
+            <li
+              key={item.id}
+              className="dropdown__item"
+              onClick={handleSeclect(item.value)}
+            >
+              {item.title}
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
