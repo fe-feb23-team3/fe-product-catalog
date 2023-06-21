3388829d367709e8cb6127b3451f4cb777a817e6
Nazarii Chepil <nazzikk1998@gmail.com>
2023-06-21 23:43:47 +0300

    implemented hover effects on contact card

--- src/Pages/Contacts/ContactCard/ContactCard.tsx
+++ src/Pages/Contacts/ContactCard/ContactCard.tsx
@@ -22,36 +22,34 @@ export const ContactCard: React.FC<Props> = ({
   github,
 }) => {
   return (
-    <div className="teamCard__container">
-      <div className="teamCard__top-wrapper">
-        <div className="image__wrapper">
-          <img
-            src={photo}
-            alt={name}
-            className="teamCard__photo"
-          />
-        </div>
+    <div className="teamCard">
+      <div className="image__wrapper">
+        <img
+          src={photo}
+          alt={name}
+          className="teamCard__photo"
+        />
+      </div>
 
-        <h2 className="teamCard__name">{name}</h2>
+      <h2 className="teamCard__name">{name}</h2>
 
-        <p className="teamCard__position">{position}</p>
+      <p className="teamCard__position">{position}</p>
 
-        <div className="teamCard__divider"></div>
+      <div className="teamCard__divider"></div>
 
-        <div className="teamCard__links">
-          <NavLink to={`tel:${phone}`} className="teamCard__links-social">
-            <i className="fa fa-phone"></i>
-          </NavLink>
-          <NavLink to={`mailto:${email}`} className="teamCard__links-social">
-            <i className="fa fa-envelope-o"></i>
-          </NavLink>
-          <NavLink to={linkedIn} className="teamCard__links-social" target="_blank">
-            <i className="fa fa-linkedin"></i>
-          </NavLink>
-          <NavLink to={github} className="teamCard__links-social" target="_blank">
-            <i className="fa fa-github"></i>
-          </NavLink>
-        </div>
+      <div className="teamCard__links">
+        <NavLink to={`tel:${phone}`} className="teamCard__links-social phone">
+          <i className="fa fa-phone"></i>
+        </NavLink>
+        <NavLink to={`mailto:${email}`} className="teamCard__links-social email">
+          <i className="fa fa-envelope-o"></i>
+        </NavLink>
+        <NavLink to={linkedIn} className="teamCard__links-social linkedin" target="_blank">
+          <i className="fa fa-linkedin"></i>
+        </NavLink>
+        <NavLink to={github} className="teamCard__links-social github" target="_blank">
+          <i className="fa fa-github"></i>
+        </NavLink>
       </div>
     </div>
   );
